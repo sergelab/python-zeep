21957082dc16948e458a4f5fd4a0667ba0722119
Serge <serge@pgtech.ru>
2017-08-18 17:40:35 +0300

    fixed version

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ with open('README.rst') as fh:
 
 setup(
     name='zeep',
-    version='2.3.0',
+    version='2.3.0am',
     description='A modern/fast Python SOAP client based on lxml / requests',
     long_description=long_description,
     author="Michael van Tellingen",
